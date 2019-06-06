91e39d5b0f74da2ab078c8a740812a9ca2cadb25
Artrix <Artrix@artrix.tech>
2019-06-06 17:10:30 +0800

    [Project] Updated features

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 import os
 
+
 def cut_string(input_str, head, tail):
     if isinstance(
-        head,
-        str) and isinstance(
-            tail,
+            head,
             str) and isinstance(
-            input_str,
-            str):
+        tail,
+        str) and isinstance(
+        input_str,
+        str):
         start = input_str.find(head) + len(head)
         end = input_str.find(tail, start)
 
@@ -25,28 +26,29 @@ def download_pic(img_url, local_url):
     with open(local_url, "wb") as file:
         file.write(img_content)
 
+
 def get_range(content):
-    cut1=cut_string(content,"td_comm5","trend")
-    cut2=cut_string(cut1,">页码：</span><span class=","/span")
-    cut3=cut_string(cut2,">","<")
+    cut1 = cut_string(content, "td_comm5", "trend")
+    cut2 = cut_string(cut1, ">页码：</span><span class=", "/span")
+    cut3 = cut_string(cut2, ">", "<")
     return int(cut3)
 
-url_base="http://zyk.ajiao.com/dzkb/"
-img_base="http://res.ajiao.com/uploadfiles/Book/"
-book_code=input("Input the book code:\n")
-content=requests.get(url_base+book_code+"_1.html").text
-page_count=get_range(content)
-print("  > This book contains "+str(page_count)+" pages.")
 
-save_dir='pics\\'+str(book_code)+"\\"
+url_base = "http://zyk.ajiao.com/dzkb/"
+img_base = "http://res.ajiao.com/uploadfiles/Book/"
+book_code = input("Input the book code:\n")
+content = requests.get(url_base + book_code + "_1.html").text
+page_count = get_range(content)
+print("  > This book contains " + str(page_count) + " pages.")
+
+save_dir = 'pics\\' + str(book_code) + "\\"
 if not os.path.exists("pics"):
     os.mkdir("pics")
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 
-for page_index in range(1,page_count+1):
+for page_index in range(1, page_count + 1):
+    img_url = img_base + book_code + "/" + str(page_index) + '.jpg'
+    print("  > Downloding pic #" + str(page_index))
 
-    img_url=img_base+book_code+"/"+str(page_index)+'.jpg'
-    print("  > Downloding pic #"+str(page_index))
-
-    download_pic(img_url, "pics\\"+str(book_code)+"\\" + str(page_index) + ".jpg")
+    download_pic(img_url, "pics\\" + str(book_code) + "\\" + str(page_index) + ".jpg")
--- names.py
+++ names.py
@@ -1,14 +1,15 @@
 import requests
 import os
 
+
 def cut_string(input_str, head, tail):
     if isinstance(
-        head,
-        str) and isinstance(
-            tail,
+            head,
             str) and isinstance(
-            input_str,
-            str):
+        tail,
+        str) and isinstance(
+        input_str,
+        str):
         start = input_str.find(head) + len(head)
         end = input_str.find(tail, start)
 
@@ -20,22 +21,29 @@ def cut_string(input_str, head, tail):
         raise TypeError("Inputs are not string!")
 
 
-url_base="http://zyk.ajiao.com/dzkb/"
+url_base = "http://zyk.ajiao.com/dzkb/"
+
 
 def get_name(content):
-    cut1=cut_string(content,"trend","td_comm5")
-    return cut_string(cut1,"n>","</s")
+    cut1 = cut_string(content, "trend", "td_comm5")
+    return cut_string(cut1, "n>", "</s")
 
-with open("")
 
-for i in range(270,500):
+def get_version(content):
+    cut1 = cut_string(content, "td_comm5", "trend")
+    cut2 = cut_string(cut1, "教育版本", "/p")
+    cut3 = cut_string(cut2, "blue\">", "<")
+    return cut3
+
+
+for i in range(270, 300):
     try:
-        url_base="http://zyk.ajiao.com/dzkb/"
-        content=requests.get(url_base+str(i)+"_1.html").text
-        print(str(i)+" > " + get_name(content))
+        url_base = "http://zyk.ajiao.com/dzkb/"
+        content = requests.get(url_base + str(i) + "_1.html").text
+        print(str(i) + " > " + get_version(content) + ": " + get_name(content))
         import time
-        time.sleep(5)
-        
+
+        # time.sleep(5)
+
     except:
         pass
-
